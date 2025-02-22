959d09f484c2e75b11d0b0e1cf3ab36b26d9cb00
ali syed <27480883-rasulkhan9210@users.noreply.replit.com>
2025-02-22 08:30:22 +0000

    basic snake

--- snake.java
+++ snake.java
@@ -29,116 +29,93 @@ public class snake {
     @Inject
     Logger log;
 
-    /**
-     * This function is called when you register your Battlesnake on play.battlesnake.com
-     * See https://docs.battlesnake.com/guides/getting-started#step-4-register-your-battlesnake
-     * <p>
-     * It controls your Battlesnake appearance and author permissions.
-     * For customization options, see https://docs.battlesnake.com/references/personalization
-     * <p>
-     * TIP: If you open your Battlesnake URL in browser you should see this data.
-     *
-     * @return Info about your snake
-     */
+    private Coord head;
+    private Coord[] body;
+    private List<String> possibleMoves;
+    private boolean hazards[][];
+
+    
     @GET
     public Info info() {
         log.infof("INFO");
         return new Info("1",
-                "",
-                "#888888",
+                "alisyed",
+                "#000000",
                 "default",
                 "default");
     }
 
-    /**
-     * This command is called once at the beginning of every game to let your
-     * Battlesnake know that a new game is about to start.
-     *
-     * @param state contains info about the boards initial state and rules
-     */
+    
     @POST
     @Path("/start")
     public void start(GameState state) {
         log.infof("%s START", state.game().id());
     }
 
-    /**
-     * This command is called once per turn of each game,
-     * providing information about the game board to your
-     * Battlesnake and asking for its next move.
-     *
-     * Your response to this command determines how your
-     * Battlesnake behaves and will be the primary focus
-     * of your game logic programming.
-     *
-     * @param state contains info about the boards current state and rules
-     * @return a Move response back for what move you wish to do: up, down, left or right.
-     */
+    
     @POST
     @Path("/move")
     public Move move(GameState state) {
-        log.infof("move called %s", state);
 
-        var head = state.you().head();
-        var body = state.you().body();
+        head = state.you().head();
+        body = state.you().body();
 
-        var possibleMoves = new ArrayList<String>(Arrays.asList("up","down","left","right"));
+        possibleMoves = new ArrayList<String>(Arrays.asList("up","down","left","right"));
+        hazards = new boolean[11][11];
 
-        // Don't allow your Battlesnake to move back in on it's own neck
-        avoidMyNeck(head, body, possibleMoves);
+        for(Battlesnake snake : state.board().snakes()){
+            for (Coord part : snake.body()){
+                hazards[part.x()][part.y()] = true;
+            }
+        }
 
-        // TODO: Using information from 'state', find the edges of the board and
-        // don't
-        // let your Battlesnake move beyond them board_height = ? board_width = ?
+        avoidObstacles();
 
-        // TODO Using information from 'state', don't let your Battlesnake pick a
-        // move that would hit its own body
+        Coord[] food = state.board().food();
+        int minimumDistance = Integer.MAX_VALUE;
 
-        // TODO: Using information from 'state', don't let your Battlesnake pick a
-        // move that would collide with another Battlesnake
+        int finalfoodx = 0;
+        int finalfoody = 0;
+        
+        for(Coord foodCoord : food){
+            int foodx = foodCoord.x();
+            int foody = foodCoord.y();
+            int distance = Math.abs(head.x() - foodx) + Math.abs(head.y() - foody);
+            if(distance < minimumDistance){
+                minimumDistance = distance;
+                finalfoodx = foodx;
+                finalfoody = foody;
+            }
+        }
 
-        // TODO: Using information from 'state', make your Battlesnake move
-        // towards a piece of food on the board
-
-        var choice = new Random().nextInt(possibleMoves.size());
-        Move move = new Move(possibleMoves.get(choice));
-
-        log.infof("MOVE %s", move);
-
+        String direction = possibleMoves.get(0);
+        
+        if(finalfoodx < head.x() && possibleMoves.contains("left")) direction = "left";
+        else if(finalfoodx > head.x() && possibleMoves.contains("right")) direction = "right";
+        else if(finalfoody < head.y() && possibleMoves.contains("down")) direction = "down";
+        else if(finalfoody > head.y() && possibleMoves.contains("up")) direction = "up";
+        
+        Move move = new Move(direction);
         return move;
     }
 
-    /**
-     * This command is called once after each game has been
-     * completed to let your Battlesnake know that the game is over.
-     *
-     * @param state contains info about the boards end state and rules
-     */
     @POST
     @Path("end")
     public void end(GameState state) {
         log.infof("%s END", state.game().id());
     }
 
-    /**
-     * Remove the 'neck' direction from the list of possible moves
-     *
-     * @param head          JsonNode of the head position e.g. {"x": 0, "y": 0}
-     * @param body          JsonNode of x/y coordinates for every segment of a
-     *                      Battlesnake. e.g. [ {"x": 0, "y": 0}, {"x": 1, "y": 0},
-     *                      {"x": 2, "y": 0} ]
-     * @param possibleMoves ArrayList of String. Moves to pick from.
-     */
-    public void avoidMyNeck(Coord head, Coord[] body, List<String> possibleMoves) {
-        var neck = body[0];
-
-        if (neck.x() < head.x()) {
+    public void avoidObstacles(){
+        if (head.x() == 0 || hazards[head.x() - 1][head.y()]){
             possibleMoves.remove("left");
-        } else if (neck.x() > head.x()) {
+        }
+        if (head.x() == hazards.length-1 || hazards[head.x() + 1][head.y()]){
             possibleMoves.remove("right");
-        } else if (neck.y() < head.y()) {
+        }
+        if (head.y() == 0 || hazards[head.x()][head.y() - 1]){
             possibleMoves.remove("down");
-        } else if (neck.y() > head.y()) {
+        }
+        if (head.y() == hazards.length-1 || hazards[head.x()][head.y() + 1]){
             possibleMoves.remove("up");
         }
     }
@@ -221,5 +198,4 @@ record Battlesnake(String id,
 
 record Coord(int x, int y) {
 }
-
 
